2fae599ef1fcd9864b3fe70927e82f4a60b9ccad
tylerburnham42 <tylerburnham42@gmail.com>
2016-04-06 22:34:34 -0400

    Not working, baudrate issues

--- arduino/Main_Sub_Code/Main_Sub_Code.ino
+++ arduino/Main_Sub_Code/Main_Sub_Code.ino
@@ -1,3 +1,7 @@
+//#define USB_CON
+//----------Includes---------------
+
+
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <Servo.h>
@@ -5,9 +9,15 @@
 ros::NodeHandle nh;
 
 
+
 std_msgs::String str_msg;
-ros::Publisher chatter("arduino_publish", &str_msg);
+ros::Publisher general_publisher("arduino_general", &str_msg);
 char charBuf[50];
+
+std_msgs::String acc_msg;
+ros::Publisher accelerometer_publisher("arduino_accelerometer", &acc_msg);
+char accelerometerBuf[50];
+
 
 Servo xLeftServo;
 Servo xRightServo;
@@ -16,7 +26,7 @@ Servo yBackServo;
 Servo zTopServo;
 Servo zBottomServo;
 
-void messageCb( const sub_message::arduino_msg& sub_message){
+void readMotorCommands( const sub_message::arduino_msg& sub_message){
   String message = String(sub_message.yBack);
 
   xLeftServo.write(sub_message.xLeft);
@@ -31,25 +41,47 @@ void messageCb( const sub_message::arduino_msg& sub_message){
   str_msg.data = charBuf;
 }
 
-ros::Subscriber<sub_message::arduino_msg> sub("arduino_move", &messageCb );
+ros::Subscriber<sub_message::arduino_msg> sub("arduino_move", &readMotorCommands);
 
-void setup()
-{ 
+void rosInit()
+{
+  nh.getHardware()->setBaud(57600);
+  //nh.getHardware()->setBaud(115200);  
+  
+  nh.initNode();
+  nh.subscribe(sub);
+
+  nh.advertise(general_publisher);
+  nh.advertise(accelerometer_publisher); 
+}
+
+void servoInit()
+{
   xLeftServo.attach(2);
   xRightServo.attach(8);
   yFrontServo.attach(13);
   yBackServo.attach(7);
   zTopServo.attach(12);
-  zBottomServo.attach(4);
+  zBottomServo.attach(4); 
+}
+
+void publishMessages()
+{
+  general_publisher.publish( &str_msg );
+  accelerometer_publisher.publish( &acc_msg );
+  nh.spinOnce();  
+}
+
+void setup()
+{ 
+  //setup_accelerometer(); 
+  rosInit();
   
-  nh.initNode();
-  nh.subscribe(sub);
-  nh.advertise(chatter);
 }
 
 void loop()
 {  
-  chatter.publish( &str_msg );
-  nh.spinOnce();
+  publishMessages();
+  //loop_accelerometer();
   delay(1);
 }
